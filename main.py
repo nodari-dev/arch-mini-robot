3ecba1e7a7b88a140a8d93c8d4405f1e108de9e6
nodari-dev1 <nodari.pylypyshak@gmail.com>
2026-02-18 23:56:59 +0100

    button + levitation

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ WHITE = st7789.color565(255, 255, 255)
 RED = 0xFF1919
 GREEN = st7789.color565(251,104,105)
 
-
 FONT_WIDTH = 8
 FONT_HEIGHT = 16
 
@@ -83,9 +82,6 @@ def boot():
     time.sleep(2)
     clear_two_whole_lines()
 
-# boot()
-
-
 buffer = bytearray(DISPLAY_W * DISPLAY_H * 2)
 
 fb = framebuf.FrameBuffer(
@@ -95,19 +91,24 @@ fb = framebuf.FrameBuffer(
     framebuf.RGB565
 )
 
-# tft.fill_rect(149, (240 - 90) // 2, 50, 90, WHITE)
-fb.fill(RED)        # black
+fb.fill(RED)
 
 tft.blit_buffer(buffer, 0, 0, DISPLAY_W, DISPLAY_H)
 
 DEFAULT_EYE_W = 60
 DEFAULT_EYE_H = 90
 
+x1=50
+x2=140
+
 class Eye:
   w = DEFAULT_EYE_W
   h = DEFAULT_EYE_H
   x = 5
   y = 120 - DEFAULT_EYE_H//2
+
+LEFT_EYE = Eye()
+RIGHT_EYE = Eye()
 
 def draw_eye(fb, x, y, w, h, r, color):
     for i in range(h):
@@ -121,37 +122,51 @@ def draw_eye(fb, x, y, w, h, r, color):
 
         fb.hline(x + r - offset, y + i, w - 2*(r - offset), color)
 
-def render_eyes(eye_height, x1=50, x2=140, y1 = 0, y2 = 0):
+def render_eyes(
+        height1=DEFAULT_EYE_H,
+        width1=DEFAULT_EYE_W,
+        height2=DEFAULT_EYE_H,
+        width2=DEFAULT_EYE_W,
+        x1=50, x2=140, y1 = 0, y2 = 0
+    ):
     fb.fill(0)
 
-    y1 = 120 - eye_height//2 if y1 == 0 else y1
-    y2 = 120 - eye_height//2 if y2 == 0 else y2
+    y1 = 120 - height1//2 if y1 == 0 else y1
+    y2 = 120 - height2//2 if y2 == 0 else y2
 
     # left eye
-    draw_eye(fb, x1, y1, 60, eye_height, 10, WHITE)
+    LEFT_EYE.x = x1
+    LEFT_EYE.y = y1
+    LEFT_EYE.w = width1
+    LEFT_EYE.h = height1
+    draw_eye(fb, x1, y1, width1, height1, 10, WHITE)
 
     # right eye
-    draw_eye(fb, x2, y2, 60, eye_height, 10, WHITE)
+    LEFT_EYE.x = x2
+    LEFT_EYE.y = y2
+    LEFT_EYE.w = width2
+    LEFT_EYE.h = height2
+    draw_eye(fb, x2, y2, width2, height2, 10, WHITE)
+
 
     tft.blit_buffer(buffer, 0, 0, DISPLAY_W, DISPLAY_H)
-
 
 def look_around():
     starter_x1 = 50
     starter_x2 = 140
-    render_eyes(90, starter_x1, starter_x2)
+    render_eyes(x1 = starter_x1, x2 = starter_x2)
     time.sleep(0.05)
     for x in range(2, 10, 2):
         starter_x1 -= x
         starter_x2 -= x
-        render_eyes(eye_height=90, x1=starter_x1, x2=starter_x2)
+        render_eyes(x1=starter_x1, x2=starter_x2)
         time.sleep(0.0001)
 
     time.sleep(0.5)
     for x in range(2, 13, 2):
         starter_x1 += x
         starter_x2 += x
-        render_eyes(eye_height=90, x1=starter_x1, x2=starter_x2)
+        render_eyes(x1=starter_x1, x2=starter_x2)
         time.sleep(0.0005)
 
     time.sleep(0.5)
@@ -159,96 +174,113 @@ def look_around():
     for x in range(2, 9, 1):
         starter_x1 -= x
         starter_x2 -= x
-        render_eyes(eye_height=90, x1=starter_x1, x2=starter_x2)
+        render_eyes(x1=starter_x1, x2=starter_x2)
         time.sleep(0.0005)
 
     time.sleep(0.5)
 
 def squeeze_eyes():
-    for h in range(90, 15, -10):
-        render_eyes(h)
+    for h in range(DEFAULT_EYE_H, 30, -5):
+        render_eyes(height1=h, height2=h)
         time.sleep(0.0001)
-    # for h in range(20, 90, 10):
-    #     render_normal_eyes(h)
-    #     time.sleep(0.0001)
+    time.sleep(3)
+    for h in range(30, 90, 5):
+        render_eyes(height1=h, height2=h)
+        time.sleep(0.0001)
 
+def susface():
+    for h in range(LEFT_EYE.h, 30, -5):
+        render_eyes(height1=h)
+        time.sleep(0.0001)
 
-def starting_eyes():
-    render_eyes(5)
+def blink():
+    for h in range(90, 5, -16):
+        render_eyes(height1=h, height2=h)
+        time.sleep(0.0001)
+    for h in range(5, 90, 16):
+        render_eyes(height1=h, height2=h)
+        time.sleep(0.0001)
+
+    time.sleep(0.05)
+    for h in range(90, 5, -16):
+        render_eyes(height1=h, height2=h)
+        time.sleep(0.0001)
+
+    for h in range(5, 90, 16):
+        render_eyes(height1=h, height2=h)
+        time.sleep(0.0001)
+
+def booting_eyes():
+    render_eyes(height1=5, height2=5)
     time.sleep(1)
 
     # try to open eyes
     for h in range(5, 20, 1):
-        render_eyes(h)
+        render_eyes(height1=h, height2=h)
         time.sleep(0.001)
 
     for h in range(20, 5, -1):
-        render_eyes(h)
+        render_eyes(height1=h, height2=h)
         time.sleep(0.001)
 
     time.sleep(1)
 
     for h in range(5, 20, 1):
-        render_eyes(h)
+        render_eyes(height1=h, height2=h)
         time.sleep(0.001)
     time.sleep(1)
 
     for h in range(20, 5, -1):
-        render_eyes(h)
+        render_eyes(height1=h, height2=h)
         time.sleep(0.001)
 
     time.sleep(2.5)
 
     # Open eyes fully
     for h in range(5, 90, 3):
-        render_eyes(h)
+        render_eyes(height1=h, height2=h)
         time.sleep(0.001)
 
-    # Blink couple of times to open eyes
     time.sleep(1)
-    for h in range(90, 5, -10):
-        render_eyes(h)
-        time.sleep(0.0001)
-    for h in range(5, 90, 10):
-        render_eyes(h)
-        time.sleep(0.0001)
-
-    time.sleep(0.05)
-    for h in range(90, 5, -10):
-        render_eyes(h)
-        time.sleep(0.0001)
-
-    for h in range(5, 90, 10):
-        render_eyes(h)
-        time.sleep(0.0001)
-
-
+    blink()
     look_around()
     squeeze_eyes()
+    susface()
 
 # boot()
-# starting_eyes()
+# booting_eyes()
 
-render_eyes(90)
+def handle_button_press():
+    button = Pin(19, Pin.IN, Pin.PULL_UP)
+    # something here
+    # while True:
+    #     if button.value() == 0:
+    #         render_text_line_center("Button yes", WHITE)
+    #         time.sleep(1)
+    #     else:
+    #         time.sleep(1)
+    #         render_text_line_center("Button not", WHITE)
 
+render_eyes()
 def levitate():
-    for h in range(5, 10, 1):
-        # render_eyes()
-        time.sleep(0.0001)
+    while True:
+        for y in range(LEFT_EYE.y, LEFT_EYE.y+10, 1):
+            render_eyes(y1=y, y2=y)
+            time.sleep(0.01)
 
+        for y in range(LEFT_EYE.y, LEFT_EYE.y-10, -1):
+            render_eyes(y1=y, y2=y)
+            time.sleep(0.01)
+
+        for y in range(LEFT_EYE.y, LEFT_EYE.y+10, 1):
+            render_eyes(y1=y, y2=y)
+            time.sleep(0.01)
+
+        for y in range(LEFT_EYE.y, LEFT_EYE.y-10, -1):
+            render_eyes(y1=y, y2=y)
+            time.sleep(0.01)
+
+
+levitate()
 # time.sleep(1)
-#
-# def susface():
-#     time.sleep(1)
-#
-# susface()
-# fb.fill(0)
 
-# draw_eye(fb, (240-80)//2, (240-80)//2, 80, 80, 15, WHITE)
-# tft.blit_buffer(buffer, 0, 0, DISPLAY_W, DISPLAY_H)
-
-# for h in range(1, 20, 1):
-#     fb.fill(0)
-#     time.sleep(0.0001)
-#     draw_eye(fb, (240-80-h)//2, (240-80-h)//2, 80-h, 80-h, 15, WHITE)
-#     tft.blit_buffer(buffer, 0, 0, DISPLAY_W, DISPLAY_H)
